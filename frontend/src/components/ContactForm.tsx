29c5b494adcab96e8d9e8c57f0b6412808df0b68
juanfer93 <jfpi_0793@hotmail.com>
2024-11-22 17:39:25 -0500

    mas cambios y cambios

--- frontend/src/components/ContactForm.tsx
+++ frontend/src/components/ContactForm.tsx
@@ -28,7 +28,7 @@ const ContactForm = () => {
   const onSubmit = async (data: FormData) => {
     setIsSubmitting(true);
     try {
-      await axios.post(`${process.env.REACT_APP_BACKEND_URL}/send-email`, data);
+      await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/send-email`, data);
       if (toast.current) {
         toast.current.show({
           severity: 'success',
