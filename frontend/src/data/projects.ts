bfca041c02305860b4e666bce5ac59d7bd2f9679
juanfer93 <jfpi_0793@hotmail.com>
2024-11-27 12:14:30 -0500

    cambios en proyecto snake en mi pagina

--- frontend/src/data/projects.ts
+++ frontend/src/data/projects.ts
@@ -8,6 +8,6 @@ export const projects: Project[] = [
   {
     title: 'Snake Game',
     description: 'Un juego clásico de la serpiente hecho con React.',
-    link: 'https://github.com/juanfer93/snake-game',
+    link: 'https://snake-game-tan-two.vercel.app/',
   },
 ];
